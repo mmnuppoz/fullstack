37386d434b1e9a61416c5b886adc6da04a26f550
mmnuppoz <mmnuppoz@student.jyu.fi>
2024-12-05 12:08:19 +0200

    tehtävä 4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -6,7 +6,28 @@ const totalLikes = (blogsArray) => {
     return blogsArray.reduce((total, blog) => total + blog.likes, 0)
 }
 
+const favoriteBlog = (blogsArray) => {
+    if (blogsArray.length === 0) {
+      return null    
+    }
+  
+    let mostLikedBlog = blogsArray[0]
+  
+    blogsArray.forEach((blog) => {
+      if (blog.likes >= mostLikedBlog.likes) {
+        mostLikedBlog = blog
+      }
+    })
+  
+    return {
+      title: mostLikedBlog.title,
+      author: mostLikedBlog.author,
+      likes: mostLikedBlog.likes
+    }
+  }
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
