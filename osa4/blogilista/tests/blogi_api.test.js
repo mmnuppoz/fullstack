2351c53347ae8d564871a1777c52412dedd5ad03
mmnuppoz <mmnuppoz@student.jyu.fi>
2024-12-05 13:07:58 +0200

    tehtävä 4.10-4.11

--- osa4/blogilista/tests/blogi_api.test.js
+++ osa4/blogilista/tests/blogi_api.test.js
@@ -31,20 +31,20 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
-test.only('there are two blogs', async () => {
+test('there are two blogs', async () => {
   const response = await api.get('/api/blogs')
 
   assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
-test.only('the first blog is about Blogipostaus1', async () => {
+test('the first blog is about Blogipostaus1', async () => {
   const response = await api.get('/api/blogs')
 
   const title = response.body.map(e => e.title)
   assert(title.includes('Blogipostaus1'))
 })
 
-test.only('blogs are returned as json', async () => {
+test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
       .expect(200)
@@ -55,8 +55,55 @@ test.only('blogs are returned as json', async () => {
     await mongoose.connection.close()
   })
 
-test.only('the indentifier field is named id'), async () => {
+test('the indentifier field is named id'), async () => {
   const response = await api.get('/api/blogs')
   const blogToCheck = response.body[0]
   expect(blogToCheck.id).toBeDefined
+}
+
+test.only('blog can be added and it is valid'), async () => {
+
+  const newBlog = {
+    title: 'Testi',
+    author: 'Kirjoittaja',
+    url: 'http.blogi.com',
+    likes: 1000
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const blogs = blogsAtEnd.map((n) => n.title)
+
+  expect(blogs).toContainEqual('Testi')
+
+}
+
+test('set likes value to 0 if not given'), async () => {
+  
+  const newBlog = {
+    title: 'Testi',
+    author: 'Kirjoittaja',
+    url: 'http.blogi.com',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const blogs = blogsAtEnd.map((n) => n.likes)
+
+  expect(blogs).toContainEqual(0)
+    
 }
